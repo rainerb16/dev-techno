318bd27443078b5cdb27cd702850650f1066be34
Rainer Brieger <hello@rainerb.com>
2020-12-12 10:41:43 -0700

    add marked library

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,20 +2,20 @@ import React from 'react';
 import marked from 'marked';
 import '../App.css';
 
-const Post = ({post}) => {
-    console.log(post)
-    const {title, featuredImage, blogContent} = post.fields
+function Post({ post }) {
+    console.log(post);
+    const { title, featuredImage, blogContent } = post.fields;
     const postContent = marked(blogContent);
 
     return (
         <div className='post'>
             <h2 className='title'>{title}</h2>
             <div className='content-container'>
-                {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={title} title={title}/>}
+                {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={title} title={title} />}
                 <article dangerouslySetInnerHTML={{ __html: postContent }} />
             </div>
         </div>
-    )
+    );
 }
 
-export default Post
+export default Post;
