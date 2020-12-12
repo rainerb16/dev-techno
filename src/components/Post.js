a8cc1778851d96919a809487f870887baf2bd03f
Rainer Brieger <hello@rainerb.com>
2020-12-12 11:45:34 -0700

    add marked library to convert markup to html

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import marked from 'marked';
 import '../App.css';
 
 function Post({ post }) {
     console.log(post);
     const { title, featuredImage, content } = post.fields;
-
+    const postContent = marked(content);
 
     return (
         <div className='post'>
             <h2 className='title'>{title}</h2>
             <div className='content-container'>
                 {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={title} title={title} />}
-                <article>{content}</article>
+                <article dangerouslySetInnerHTML={{__html: postContent}} />
             </div>
         </div>
     );
