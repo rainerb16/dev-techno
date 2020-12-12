f0d23ade945ef2ad207160a13e1c66bd5300f6e0
Rainer Brieger <hello@rainerb.com>
2020-12-12 10:38:18 -0700

    refactor code for dangerouslysethtml

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import marked from 'marked';
 import '../App.css';
 
 const Post = ({post}) => {
     console.log(post)
     const {title, featuredImage, blogContent} = post.fields
+    const postContent = marked(blogContent);
+
     return (
         <div className='post'>
             <h2 className='title'>{title}</h2>
             <div className='content-container'>
                 {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={title} title={title}/>}
-                <article>
-                    {blogContent}
-                </article>
+                <article dangerouslySetInnerHTML={{ __html: postContent }} />
             </div>
         </div>
     )
