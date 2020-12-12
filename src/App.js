b562ea886096ab6a01b753dc14358e37d85997df
Rainer Brieger <hello@rainerb.com>
2020-12-12 09:08:16 -0700

    Initial commit, added code to make blog look pretty, added API from contentful

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { client } from './client';
 import Posts from './components/Posts';
 
+
 class App extends React.Component {
 	state = {
 		posts: []
@@ -24,12 +25,19 @@ class App extends React.Component {
 			<div className="App">
 				<div className="main-container">
 					<header>
-						<div className="wrapper">
-							<h1>DEV.TECHNO</h1>
+						<div className='wrapper'>
+							<h1 className="devtechno">DEV.TECHNO</h1>
+						</div>
+						<div className='site-links'>
+							<a className='link-item' href="https://www.rainerb.ml">HOME</a>
+							<a className='link-item' href="https://www.rainerb.ml/#/about">ABOUT</a>
+							<a className='link-item' href="https://klvsmusic.bandcamp.com/">BANDCAMP</a>
+							<a className='link-item' href="https://www.obskurmusic.co/">OBSKUR MUSIC [LABEL]</a>
+							<span></span>
 						</div>
 					</header>
 					<main>
-						<div className="post-container">
+						<div className='post-container'>
 							<Posts posts={this.state.posts} />
 						</div>
 					</main>
